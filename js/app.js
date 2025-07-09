d05f11444c2acc1bf08a9ba82c3cfc3cf5ed9797
yousif <138237408+yousifj129@users.noreply.github.com>
2025-07-09 19:17:51 +0300

    done the things

--- js/app.js
+++ js/app.js
@@ -1,88 +1,85 @@
+let board = ['', '', '', '', '', '', '', '', '']
+let turn = "X"
+let previousTurn = "O"
+let winner = ""
+let tie = false
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+]
 function init() {
 
 
     /*-------------------------------- Constants --------------------------------*/
-    const squareEls = document.querySelectorAll(".sqr")
-    const messageEl = document.querySelector("#message")
+
 
     /*---------------------------- Variables (state) ----------------------------*/
-    let board = ['', '', '', '', '', '', '', '', '']
-    let turn = "X"
-    let winner = false
-    let tie = false
-    const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8]
-    ]
+
 
 
     /*------------------------ Cached Element References ------------------------*/
-
+    const squareEls = document.querySelectorAll(".sqr")
+    const messageEl = document.querySelector("#message")
 
 
     /*-------------------------------- Functions --------------------------------*/
     function haveSameElements(arr1, arr2) {
         for (let i = 0; i < arr2.length; i++) {
-            if(!arr1.includes(arr2[i])){
+            if (!arr1.includes(arr2[i])) {
                 return false;
             }
         }
         return true;
     }
-    function checkForWinner(){
+    function checkForWinnerAndTie() {
         let a = []
         tie = true;
         for (let i = 0; i < board.length; i++) {
-            if(board[i] == "X"){
+            if (board[i] == previousTurn) {
                 a.push(i);
             }
         }
         console.log(a)
-        winningCombos.forEach(function(e){
-            if(haveSameElements(a.sort(),e.sort())){
+        winningCombos.forEach(function (e) {
+            if (haveSameElements(a.sort(), e.sort())) {
                 console.log("done")
+                winner = previousTurn
+                tie = false;
             }
         })
         for (let i = 0; i < board.length; i++) {
-            if(board[i] == ""){
+            if (board[i] == "") {
                 tie = false;
             }
         }
-        
-    }
-    function checkForTie(){
 
     }
     function placePiece(index) {
         board[index] = turn
+        previousTurn = turn
         if (turn == "X") {
             turn = "O"
         }
-        else{
-            
+        else {
+
             turn = "X"
         }
-        const win = checkForWinner()
+        checkForWinnerAndTie()
 
-        if(winner != ""){
-            messageEl.textContent = `Player ${winner} wins!`
-        }
-        else if(tie){
-            messageEl.textContent = `it's a tie!`
-        }
-        else{
-            messageEl.textContent = "Player 2 wins!"
-        }
+
 
 
     }
     function handleClick(event) {
+        if (winner != "" || tie == true) {
+            return;
+        }
         const squareIndex = event.target.id
         console.log(squareIndex)
         if (board[squareIndex] == "X" || board[squareIndex] == "O") {
@@ -98,6 +95,13 @@ function init() {
         else if (tie == true) {
             messageEl.textContent = `it's a tie!`
         }
+        if (winner != "") {
+            messageEl.textContent = `Player ${winner} wins!`
+        }
+        else if (tie) {
+            messageEl.textContent = `it's a tie!`
+        }
+
     }
     function updateBoard() {
         for (let i = 0; i < board.length; i++) {
@@ -113,10 +117,20 @@ function init() {
         })
     }
 
+    function resetGame() {
+        board = ['', '', '', '', '', '', '', '', '']
+        turn = "X"
+        previousTurn = "O"
+        winner = ""
+        tie = false
+        updateBoard()
+        updateMessage()
+    }
 
 
     /*----------------------------- Event Listeners -----------------------------*/
     render()
+    document.querySelector("#reset").addEventListener("click", resetGame)
 }
 
 
